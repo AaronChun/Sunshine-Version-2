1196c36037a5c8a6cc96a325cc268aa6e1e0a849
AaronChun <aichanchun@gmail.com>
2017-01-21 23:37:31 +0800

    修改SettingsActivity类，加载pref_general.xml配置

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -14,8 +14,9 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
 
 
     private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(this);
-
+        // Trigger the listener immediately with the preference's current value.
         onPreferenceChange(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
 
     }
@@ -25,6 +26,9 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        addPreferencesFromResource(R.xml.pref_general);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
 
     }
 
