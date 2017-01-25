a2450914bf7796f1394a6ce13f235dc19a96b065
AaronChun <aaroncchun@outlook.com>
2017-01-25 17:08:58 +0800

    添加选择温度的设置Preference

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -142,7 +142,7 @@ public class ForecastFragment extends Fragment {
     private String getReadableDateString(long time) {
       // Because the API returns a unix timestamp (measured in seconds),
       // it must be converted to milliseconds in order to be converted to valid date.
-      SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+      SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("yyyy-MM-dd E a");
       return shortenedDateFormat.format(time);
     }
 
@@ -150,7 +150,14 @@ public class ForecastFragment extends Fragment {
     /**
      * Prepare the weather high/lows for presentation.
      */
-    private String formatHighLows(double high, double low) {
+    private String formatHighLows(double high, double low, String unitType) {
+      if (unitType.equals(getString(R.string.pref_units_imperial))) {
+        high = (high * 1.8) + 32;
+        low = (low * 1.8) + 32;
+      } else if (!unitType.equals(getString(R.string.pref_units_imperial))) {
+        Log.d(LOG_TAG, "Unit type not found: " + unitType);
+      }
+
       // For presentation, assume the user doesn't care about tenths of a degree.
       long roundedHigh = Math.round(high);
       long roundedLow = Math.round(low);
@@ -188,6 +195,16 @@ public class ForecastFragment extends Fragment {
 
       String[] resultStrs = new String[numDays];
 
+      // Data is fetched in Celsius by default.
+      // If user prefers to see in Fahrenheit, convert the values here.
+      // We do this rather than fetching in Fahrenheit so that the user can
+      // change this option without us having to re-fetch the data once
+      // we start storing the values in a database.
+
+      SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+      String unitType = sharedPreferences.getString(getString(R.string.pref_units_key),getString(R.string.pref_units_metric));
+
       for (int i = 0; i < weatherArray.length(); i++) {
         // For now, using the format "Day, description, hi/low"
         String day;
@@ -215,7 +232,7 @@ public class ForecastFragment extends Fragment {
         double high = temperatureObject.getDouble(OWM_MAX);
         double low = temperatureObject.getDouble(OWM_MIN);
 
-        highAndLow = formatHighLows(high, low);
+        highAndLow = formatHighLows(high, low,unitType);
         resultStrs[i] = day + " - " + description + " - " + highAndLow;
       }
 
@@ -248,7 +265,7 @@ public class ForecastFragment extends Fragment {
 
         final String FORCAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
 
-        final String QUERY_PARAM = "q";
+        final String QUERY_PARAM = "id";
         final String FORMAT_PARAM = "mode";
         final String UNITS_PARAM = "units";
         final String DAYS_PARAM = "cnt";
